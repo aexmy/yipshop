85f8a7a312fa638d288396da8640d9bbfcfb6a3c
amy <amy.yip@hotmail.com>
2019-09-13 09:44:52 -0400

    created card_contents (3)

--- app/src/main/java/com/example/yipshop/MenuActivity.java
+++ app/src/main/java/com/example/yipshop/MenuActivity.java
@@ -1,13 +1,25 @@
 package com.example.yipshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = CheckoutActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void launchCheckoutActivity(View view) {
+        Intent intent = new Intent(this, CheckoutActivity.class);
+        startActivity(intent);
+
+        Log.d(LOG_TAG, "Button clicked!");
     }
 }
