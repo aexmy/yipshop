ae771e61c1751191ad60035b361cdc0064c3593c
amy <amy.yip@hotmail.com>
2019-09-17 17:14:24 -0400

    added decrease method. still have to truncate to 2 decimals, and make it work for the 2 other objects

--- app/src/main/java/com/example/yipshop/MenuActivity.java
+++ app/src/main/java/com/example/yipshop/MenuActivity.java
@@ -1,6 +1,7 @@
 package com.example.yipshop;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +17,7 @@ public class MenuActivity extends AppCompatActivity {
     private static final String LOG_TAG = CheckoutActivity.class.getSimpleName();
     private static final String LOG_TAGR = MenuActivity.class.getSimpleName();
     int counter = 0;
+
     private TextView price;
     private TextView quantity;
     private TextView subtotal;
@@ -25,47 +27,108 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        final Button button = findViewById(R.id.add_button);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button button_increase = findViewById(R.id.add_button);
+        button_increase.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Code here executes on main thread after user presses button
                 ViewGroup temp = (ViewGroup) v.getParent();
 
                 //get the price, child 3
                 TextView price = (TextView) temp.getChildAt(3);
                 String p = price.getText().toString();
                 double pp = Double.parseDouble(p);
-                price.setText("" + pp);
-                Log.d(LOG_TAGR, "*price*" + pp);//*how do i convert view to double?*
+                Log.d(LOG_TAGR, "*price*" + pp);
 
                 //get the quantity, child 10
                 TextView quantity = (TextView) temp.getChildAt(10);
                 String q = quantity.getText().toString();
                 int qq = Integer.parseInt(q);
-                quantity.setText("" + qq);
                 counter = counter + 1;
                 display(counter);
-                Log.d(LOG_TAGR, "*quantity*" + qq);//*how do i convert view to int?*
+                Log.d(LOG_TAGR, "*quantity*" + counter);
 
                 //get the subtotal, child 11
                 TextView subtotal = (TextView) temp.getChildAt(11);
                 String s = subtotal.getText().toString();
                 double ss = Double.parseDouble(s);
-                subtotal.setText("" + ss);
-                displaySub(counter*pp);
-                Log.d(LOG_TAGR, "*subtotal*" + ss);
-
-                //public void increase(){}
+                double finalSubtotal = counter * pp;
+                displaySub(finalSubtotal);
+                Log.d(LOG_TAGR, "*subtotal*" + finalSubtotal);
 
             }
-            /**
+
+            private void display(int number) {
+                TextView displayInteger = (TextView) findViewById(R.id.integer);
+                displayInteger.setText("" + number);
+            }
+
+            private void displaySub(double number) {
+                TextView displaySubtotal = (TextView) findViewById(R.id.subtotal);
+                displaySubtotal.setText("" + number);
+            }
+
+        });
+
+        final Button button_decrease = findViewById(R.id.minus_button);
+        button_decrease.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ViewGroup temp = (ViewGroup) v.getParent();
+
+                //get the price, child 3
+                TextView price = (TextView) temp.getChildAt(3);
+                String p = price.getText().toString();
+                double pp = Double.parseDouble(p);
+                //display
+                Log.d(LOG_TAGR, "*price*" + pp);
+
+                //get the quantity, child 10
+                TextView quantity = (TextView) temp.getChildAt(10);
+                String q = quantity.getText().toString();
+                int qq = Integer.parseInt(q);
+                if(counter > 0) {
+                    counter = counter - 1;
+                }
+                //display
+                display(counter);
+                Log.d(LOG_TAGR, "*quantity*" + qq);
+
+                //get the subtotal, child 11
+                TextView subtotal = (TextView) temp.getChildAt(11);
+                String s = subtotal.getText().toString();
+                double ss = Double.parseDouble(s);
+                double finalSubtotal = counter * pp;
+                //display
+                displaySub(finalSubtotal);
+                Log.d(LOG_TAGR, "*subtotal*" + finalSubtotal);
+
+            }
+
+            private void display(int number) {
+                TextView displayInteger = (TextView) findViewById(R.id.integer);
+                displayInteger.setText("" + number);
+            }
+
+            private void displaySub(double number) {
+                TextView displaySubtotal = (TextView) findViewById(R.id.subtotal);
+                displaySubtotal.setText("" + number);
+            }
+        });
+    }
+
+    public void launchCheckoutActivity(View view) {
+        Intent intent = new Intent(this, CheckoutActivity.class);
+        startActivity(intent);
+
+        Log.d(LOG_TAG, "Button clicked!");
+    }
+}
+/**
             public void increase(View view){
                 @Override
                 public void onClick(View v){
 
                 }
             }
-**/
+
             private TextView getPriceFromButton(View view){
                 ViewGroup temp = (ViewGroup)view.getParent();
                 TextView price = (TextView)temp.getChildAt(3);
@@ -94,7 +157,7 @@ public class MenuActivity extends AppCompatActivity {
         });
 
     }
-/**
+
     public void increase(View view){
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -105,22 +168,7 @@ public class MenuActivity extends AppCompatActivity {
         });
     }
  **/
-    private void display(int number) {
-        TextView displayInteger = (TextView) findViewById(R.id.integer);
-        displayInteger.setText("" + number);
-    }
 
-    private void displaySub(double number){
-        TextView displaySubtotal = (TextView) findViewById(R.id.subtotal);
-        displaySubtotal.setText("" + number);
-    }
-
-    public void launchCheckoutActivity(View view) {
-        Intent intent = new Intent(this, CheckoutActivity.class);
-        startActivity(intent);
-
-        Log.d(LOG_TAG, "Button clicked!");
-    }
 /**
     public void button_id(View view){
         //get the parent view which is the constraint layout
@@ -165,4 +213,4 @@ public class MenuActivity extends AppCompatActivity {
         return quantity;
     }
 **/
-}
+
