ea61d8465cdb77c261c2f2efc716caa5776b3764
amy <amy.yip@hotmail.com>
2019-09-16 23:45:20 -0400

    attempted to configure the button > to retrieve the price, quantity and subtotal. not much progress

--- app/src/main/java/com/example/yipshop/MenuActivity.java
+++ app/src/main/java/com/example/yipshop/MenuActivity.java
@@ -6,17 +6,37 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MenuActivity extends AppCompatActivity {
 
-    //int minteger;
     private static final String LOG_TAG = CheckoutActivity.class.getSimpleName();
+    private static final String LOG_TAGR = MenuActivity.class.getSimpleName();
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        /**
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                //get the parent view which is the constraint layout
+                ViewGroup temp = (ViewGroup)v.getParent();
+
+                //get the price, child 3
+                View price = temp.getChildAt(3);
+                Log.d(LOG_TAGR, "*price*"+price);//*how do i convert view to double?*
+
+                //get the quantity, child 10
+                View quantity = temp.getChildAt(10);
+                Log.d(LOG_TAGR, "*quantity*"+quantity);//*how do i convert view to int?*
+            }
+        });
+         **/
     }
 
     public void launchCheckoutActivity(View view) {
@@ -26,46 +46,37 @@ public class MenuActivity extends AppCompatActivity {
         Log.d(LOG_TAG, "Button clicked!");
     }
 
-    /**
-    private void buy(View view){
-        ViewGroup viewer = (ViewGroup)view.getParent();
-        viewer.getChildAt(3);
-        Log.d(LOG_TAG, "Increased! Decreased!");
+    public void buy(View view){
+        //get the parent view which is the constraint layout
+        ViewGroup temp = (ViewGroup)view.getParent();
+
+        View v = getPriceFromButton(view);
+        Log.d(LOG_TAGR, "*33price*"+v);
+
+        //get the price, child 3
+        View price = temp.getChildAt(3);
+        Log.d(LOG_TAGR, "*price*"+price);//*how do i convert view to double?*
+
+        //get the quantity, child 10
+        View quantity = temp.getChildAt(10);
+        Log.d(LOG_TAGR, "*quantity*"+quantity);//*how do i convert view to int?*
     }
 
-    public void increase(View view){
-        minteger = minteger + 1;
-        display(minteger);
+
+    private TextView getPriceFromButton(View view){
+        ViewGroup temp = (ViewGroup)view.getParent();
+        return (TextView)temp.getChildAt(3);
     }
 
-    public void decrease(View view){
-        if(minteger > 0)
-            minteger = minteger - 1;
-        display(minteger);
+    private TextView getSubtotalFromButton(View view){
+        ViewGroup temp = (ViewGroup)view.getParent();
+        return (TextView)temp.getChildAt(11);
     }
 
-    private void display(int number){
-        TextView displayInteger = (TextView)findViewById(R.id.integer);
-        displayInteger.setText("" + number);
+    private TextView getQuantityFromButton(View view){
+        ViewGroup temp = (ViewGroup)view.getParent();
+        return (TextView)temp.getChildAt(10);
+
     }
 
-    private double getPrice(){
-        TextView displayPrice = (TextView)findViewById(R.id.price);
-        double desiredPrice = Integer.valueOf(displayPrice.getText().toString());
-        return desiredPrice;
-    }
-
-    private int getQuantity(){
-        TextView displayQuantity = (TextView)findViewById(R.id.quantity);
-        int desiredQuantity = Integer.valueOf(displayQuantity.getText().toString());
-        return desiredQuantity;
-    }
-
-    private void displaySubtotal(int subtotal){
-        TextView textView = (TextView)findViewById(R.id.subtotal);
-        double displaySubtotal = getPrice()*getQuantity();
-        textView.setText(String.valueOf(displaySubtotal));
-        textView.setText(""+displaySubtotal);
-    }
-     **/
 }
